1739d3a1bea5d8a3b135e8329084a28516afe286
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2023-03-27 11:18:23 -0400

    Gradle refactoring

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,14 +1,3 @@
-plugins {
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
-    
-}
-
 dependencies {
-    implementation("com.google.guava:guava:31.1-jre")
 }
 
-application {
-    // Define the main class for the application.
-    mainClass.set("com.yeti.hexarch.usermgmt.domain.App")
-}
