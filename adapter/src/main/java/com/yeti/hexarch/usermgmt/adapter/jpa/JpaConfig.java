0809d097bbcf1176d42b2fcda2ffc30c963d95ca
sachindra-maharjan <118582768+sachindra-maharjan@users.noreply.github.com>
2023-03-29 17:58:42 +0000

    added docker for mongo

---
+++ adapter/src/main/java/com/yeti/hexarch/usermgmt/adapter/jpa/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.yeti.hexarch.usermgmt.adapter.jpa;
+
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@EnableMongoRepositories
+public class JpaConfig {
+    
+}
